a20f5787e30bc49751721d09fa169328f9613f37
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-08 07:03:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 
 
 def meal_choice (veg1, veg2, protein = 'tofu')
- "A plate of #{protein} with #{veg1} and #{veg2}."
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts "What a nutritious meal!"
 end
 
